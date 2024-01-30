886941a2175398e6be20b5d0b49fee9b401128fe
arshiwang <shiwangaryan@gmail.com>
2024-01-30 16:07:09 +0530

    started with the homepage & basic navbar- Next|Tailwind

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Porvider from '@components/Provider';
 
 export const metadata = {
   title: "Promptopia",
@@ -11,9 +13,10 @@ const Layout = ({ children }) => {
         <div className="main">
           <div className="gradient" />
         </div>
+        <main className="app">
+          <Nav/>
+          {children}</main>
       </body>
-
-      <main className="app">{children}</main>
     </html>
   );
 };
