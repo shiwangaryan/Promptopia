1dc8250af798edbd68e85e5d09c5faf2dec8e410
arshiwang <shiwangaryan@gmail.com>
2024-02-02 20:46:45 +0530

    created route, user schema and other required files for allowing user sign in and storing info in a DB- Next|MongoDB|Mongoose

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,38 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
-const handler= NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    async session({session}) {
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
 
-    },
-    async signIn({profile}) {
-        tr 
+      //user exits
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+      //user doesnt exit
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-})
+  },
+});
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
